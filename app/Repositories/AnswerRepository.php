897e543616c236670e6e3b8d06776b836a2dc616
XBisATrouble <XBisATrouble@gmail.com>
2017-05-01 23:07:35 +0800

    first commit

---
+++ app/Repositories/AnswerRepository.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 10656
+ * Date: 2017/5/1
+ * Time: 1:54
+ */
+
+namespace App\Repositories;
+
+
+use App\Answer;
+
+class AnswerRepository
+{
+    public function create(array $attributes)
+    {
+        return Answer::create($attributes);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
+
+Route::get('/topics', function (Request $request) {
+    $topics=\App\Topic::select(['id','name'])->where('name','like','%'.$request->query('q').'%')->get();
+    return $topics;
+})->middleware('api');
+
+Route::post('/question/follower', function (Request $request) {
+    $user = Auth::guard('api')->user();
+    $followed=$user->followed($request->get('question'));
+    if ($followed){
+        return response()->json(['followed'=>true]);
+    }
+    return response()->json(['followed'=>false]);
+})->middleware('auth:api');
+
+Route::post('/question/follow', function (Request $request) {
+    $user = Auth::guard('api')->user();
+    $question=\App\Question::find($request->get('question'));
+    $followed=$user->followThis($question->id);
+
+    if(count($followed['detached']) > 0){
+        $question->decrement('followers_count');
+        return response()->json(['followed'=>false]);
+    }
+
+    $question->increment('followers_count');
+    return response()->json(['followed'=>true]);
+})->middleware('auth:api');
